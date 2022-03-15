e81f68148e05423e5ae5337f26ea6d8aee221598
JuliaKucmierczyk <89534632+JuliaKucmierczyk@users.noreply.github.com>
2022-03-15 09:17:15 +0100

    Laboratoria drugie porzadkowanie

---
+++ lab2-zadania/zad1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace zad1
+{
+    class zad1
+    {
+        static void Main()
+        {
+            int[] oceny = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine(suma(oceny));
+
+            int suma(int[] tablica)
+            {
+                int l = tablica.Length;
+                int suma = 0;
+
+                for (int i = 0; i < l; i++)
+                {
+                    suma += tablica[i];
+                }
+                return suma;
+
+            }
+        }
+    }
+}
+
+
---
+++ lab2-zadania/zad3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace zad3
+{
+    class zad3
+    {
+        static void Main()
+        {
+
+            for (int i = 1; i < 101; i++)
+            {
+                if (i - 5 * (i / 5) == 0 && i - 3 * (i / 3) == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else
+                     if (i - 5 * (i / 5) == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                    if (i - 3 * (i / 3) == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                    Console.WriteLine(i);
+            }
+
+        }
+    }
+}
